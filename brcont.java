0d9db66d19652b2a8e85253f6e5d9f94a17d028a
SILVXT-AJ <silvest43n85@gmail.com>
2025-07-07 15:18:34 +0530

    it's a brcont programming file .java

---
+++ brcont.java
@@ -0,0 +1,16 @@
+public class brcont {
+    public static void main (String [] args){
+
+        for (int i =1; i <= 10 ;i++ ){
+
+            if(i == 8){
+                continue;
+            }
+
+
+            System.out.print(i + " ");
+        }
+
+
+    }
+}
